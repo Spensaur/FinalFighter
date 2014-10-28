fcb92db94060dddf51c38808935eb7dbe684eb1b
brandonc91 <brandonc91@gmail.com>
2014-10-27 22:09:11 -0700

    Empty button and two background images

---
+++ NewStages/Assets/GameSetup.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSetup : MonoBehaviour
+{
+		public bool buttonState;
+		public string textFieldState;
+
+		// Use this for initialization
+		void Start ()
+		{
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+
+		void OnGUI ()
+		{
+		
+				int xOffset = (Screen.width - 150) / 2;
+				int yOffset = (Screen.height / 2) + 50;
+				Rect pos = new Rect (xOffset, yOffset, 150, 20);
+				//		Screen.width / 2, (Screen.height / 2) - 10
+				pos.y += 30;
+		
+				if (GUI.Button (pos, "Play Game")) {
+						buttonState = true;
+				}
+				pos.y += 30;
+		
+				textFieldState = GUI.TextField (pos, textFieldState);
+				pos.y += 30;
+		}
+}
